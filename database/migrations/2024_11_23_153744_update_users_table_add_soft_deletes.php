fc6d5bebe32b3e83248350852d3d0a05231c6735
YuiMigaki <yuimigaki1221@icloud.com>
2024-11-25 14:43:40 +0800

    feat(authentication): Add authenticated access to features

---
+++ database/migrations/2024_11_23_153744_update_users_table_add_soft_deletes.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Assessment Title: Portfolio Part 3
+ * Cluster:          Cluster - SaaS: Front-End Dev - ICT50220 (Advanced Programming)
+ * Qualification:    ICT50220 Diploma of Information Technology (Back End Web Development)
+ * Name:             Yui Migaki
+ * Student ID:       20098757
+ * Year/Semester:    2024/S2
+ *
+ * YOUR SUMMARY OF PORTFOLIO ACTIVITY
+ * This portfolio is based on a scenario where I am employed as a Junior Web Application Developer at RIoT Systems,
+ * a Perth-based company specializing in IoT, Robotics, and Web Application systems. My task is to implement
+ * a simple web application using PHP and elements of the MVC (Model-View-Controller) development methodology.
+ * The process involves following a predefined set of steps, with opportunities to consult stakeholders or their representatives for guidance.
+ * The ultimate goal is to develop a web application that aligns with the company's expertise in IoT, Robotics, and Web
+ *
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->softDeletes();});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropSoftDeletes();});
+
+    }
+};
